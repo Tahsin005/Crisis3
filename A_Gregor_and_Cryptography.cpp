8918a4c251dea670fa52f3d7d1c02b662ca14705
MD. Tahsin Ferdous <124136023+Tahsin005@users.noreply.github.com>
2024-05-30 00:14:49 +0600

    Add files via upload

---
+++ A_Gregor_and_Cryptography.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+     int tt; cin >> tt; while (tt--) {
+          long long n; cin >> n;
+          cout << 2 << " " << n - 1 << '\n';
+     }
+}
